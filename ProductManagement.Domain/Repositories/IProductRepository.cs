771354cf17d21fac2a856d1662d154958687cbf2
Mohamed EL HOSAYNY <mohamed.elhosayny@artza-technologies.com>
2021-05-03 09:53:32 +0000

    Add product repository

---
+++ ProductManagement.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetAll();
+        Product Add(Product product);
+    }
+}
---
+++ ProductManagement.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using ProductManagement.Domain.Entities;
+using ProductManagement.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Product> GetAll()
+        {
+            return _context.Products;
+        }
+
+        public Product Add(Product product)
+        {
+            return _context.Products.Add(product);
+        }
+    }
+}
