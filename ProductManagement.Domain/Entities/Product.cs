66c76c7e53628793dbc3396f69ecaab4edb2b714
Mohamed EL HOSAYNY <mohamed.elhosayny@artza-technologies.com>
2021-05-03 09:22:21 +0000

    Add product entity

---
+++ ProductManagement.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Code { get; set; }
+        public DateTime DateDebutValidation { get; set; }
+        public DateTime DateFinValidation { get; set; }
+    }
+}
